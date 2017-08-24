47d4a81a5884181417ba0603ea77209cd4b87dba
Carlos Eduardo Ferreira Marques <carlos.marques@SUPERNOSSO.INTRA>
2017-08-23 22:37:26 -0300

    Secao 1 - Part 1 - Fundamentos_Linguagem_JavaScript

---
+++ curso-de-javascript-completo/js/ParteI_Fundamentos_Linguagem_JavaScript.js
@@ -0,0 +1,60 @@
+
+
+/*var vNome = prompt("Qual seu nome?")
+var vIdade =prompt("Qual sua idade?");
+
+
+resultado = "O cliente "+vNome+" tem "+vIdade+" anos.";
+
+alert(resultado);
+
+console.log(resultado);*/
+
+/*var nome = "Manuel",
+idade = 39,
+str ;
+
+console.log(typeof(nome));*/
+
+/*var nome = "Manuel",
+idade = 39,
+valido = true,
+texto = "carlos",
+reg = RegExp(),
+vetor = Array(),
+obj = new Object(),
+funcao = function() {return 0};
+
+console.log("nome = " + typeof(nome));
+console.log("idade = " +typeof(idade));
+console.log("valido = " +typeof(valido));
+console.log("reg = " +typeof(reg));
+console.log("vetor = " +typeof(vetor));
+console.log("obj = " +typeof(obj));
+console.log("funcao = " +typeof(funcao));
+
+if(texto)
+    console.log("verdadeiro");
+else
+    console.log("false");*/
+
+var a = 9,
+    b = 2;
+    soma = a + b;
+    subtracao = a - b;
+    multiplicacao = a * b;
+    divisao = a / b;
+    resto = a%b;
+
+  //operadores
+  console.log("Valor de a= "+a);
+  console.log("Valor de b= "+b);
+  console.log("Valor de soma= "+soma);
+  console.log("Valor de subtracao= "+subtracao);
+  console.log("Valor de multiplicacao= "+multiplicacao);
+  console.log("Valor de divisao= "+divisao);
+  console.log("Valor de resto= "+resto);
+
+  
+
+
